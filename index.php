84588d12cde6ffd0d6de61d964dae311c8875981
pondjungzah <naipond_zaa>
2016-05-12 23:53:41 +0700

    landing page

--- index.php
+++ index.php
@@ -4,8 +4,11 @@
 	<title>
 		KUY
 	</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/app.css">
 </head>
 <body>
-
+	<div class="landing-container">
+	<img src="assets/images/landing/image-landing-1-master.jpg">
+	</div>
 </body>
 </html>
